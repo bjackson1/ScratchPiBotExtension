f7a61fa1183844ee1271e653bad7de36c430663f
Brett <Brett.Jackson@hscic.gov.uk>
2016-05-25 14:54:11 +0100

    Added time functions

--- scratchhelper.py
+++ scratchhelper.py
@@ -5,6 +5,7 @@ from socket import gethostname
 from threading import Thread
 
 from time import sleep
+import urllib2
 
 
 app = Flask(__name__)
@@ -19,15 +20,34 @@ def poll():
   if busyId > -1:
     ret = "_busy " + str(busyId)
 
+  print(ret)
   return ret
   
-@app.route('/move/<wheel>/<speed>')
-def move(wheel, speed):
+#  127.0.0.1 - - [14/May/2016 10:45:13] "GET /move/9/both/forward/6/fast HTTP/1.1" 404 -
+
+@app.route('/move/<id>/<wheel>/<direction>/<time>/<speed>')
+def move(id, wheel, direction, time, speed):
+  global busyId
+  busyId = id
+  res = urllib2.urlopen('http://192.168.1.68:5000/move/' + wheel + '/' + direction + '/' + time + '/' + speed)
+  busyId = -1
+
+  print(res)
+  return res
+
+@app.route('/turn/<id>/<turndirection>/<time>/<speed>')
+def turn(id, turndirection, time, speed):
+  global busyId
+  busyId = id
+  urllib2.urlopen('http://192.168.1.68:5000/turn/' + turndirection + '/' + time + '/' + speed)
+  busyId = -1
+
   return "_result 0"
 
 @app.route('/stop/<wheel>')
 def stop(wheel):
+  urllib2.urlopen('http://192.168.1.68:5000/stop/' + wheel)
   return "_result 0"
 
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0', threaded=True)
+  app.run(debug=False, host='0.0.0.0', threaded=True)
